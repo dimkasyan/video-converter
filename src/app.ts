83ee8f473ef9ddd1e7b2dce9cea9c06eee805c39
Dmitriy Petrov <d.petrov@polymatica.com>
2022-04-24 01:53:47 +0300

    add input processing

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { PromptSrevice } from "./core/prompt.service";
+
+export class App {
+   async run() {
+        const res = await (new PromptSrevice()).input<number>('Число', 'number');
+        console.log(res);
+    }
+}
+
+const app = new App();
+app.run();
